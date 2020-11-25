fc18de40fd21db66f825200a2d9b123de878cc79
Passive1412 <marcusjhomann@outlook.com>
2020-11-25 08:53:50 +0100

    some more dependecies and work on getting auth and error handling in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
+// Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// AgGrid Modules
+import { AgGridModule } from '@ag-grid-community/angular';
+
+// App Modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Environment
+import { environment } from 'src/environments/environment';
+
+// NG Modules
+import {
+  NgbDateAdapter,
+  NgbDateNativeUTCAdapter,
+  NgbModule,
+} from '@ng-bootstrap/ng-bootstrap';
+
+// Core Modules
+
+// Page Modules
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    { provide: ErrorHandler, useClass: environment.errorHandler },
+    // { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+    { provide: NgbDateAdapter, useClass: NgbDateNativeUTCAdapter },
+
+    //CanActivateGuardService
+    //Authservice
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [],
 })
-export class AppModule { }
+export class AppModule {}
